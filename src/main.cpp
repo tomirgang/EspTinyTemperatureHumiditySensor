f6c7dea0c67a4b08ec5d71107ba0268eec79af28
Tom <thomas@irgang.eu>
2023-01-21 10:27:48 +0100

    test code and fix lib config

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include "DHT.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define DHT_TYPE DHT22
+#define PIN_DHT22 2
+
+DHT dht(PIN_DHT22, DHT_TYPE);
+
+float humidity;
+float temperature;
+
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    humidity = dht.readHumidity();
+    temperature = dht.readTemperature();
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%, Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+
+    delay(1000);
 }
