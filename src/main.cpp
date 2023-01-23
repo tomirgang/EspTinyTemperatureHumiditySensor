2d8ffc92a11f90feac152c7735551ef661a5bddc
Tom <thomas@irgang.eu>
2023-01-23 12:32:29 +0100

    bugs fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <time.h>
 #include <sensor_constants.h>
 #include <serial_helper.h>
 #include <data_helper.h>
@@ -9,14 +10,21 @@
 #define PIN_DHT22 2
 #define DHT_TYPE DHT22
 
+bool read_sensor_values(void);
+
 DHT dht(PIN_DHT22, DHT_TYPE);
 
-void read_sensor_values(void);
+time_t last_measurement = 0;
+bool ntp_setup = false;
 
 void setup()
 {
     Serial.begin(115200);
     dht.begin();
+
+    wait_for_serial_connect();
+
+    init_data();
 
     wait_for_serial_connect();
 
@@ -39,47 +47,50 @@ void setup()
         wifi_connect();
     }
 
-    if(WiFi.isConnected()) {
+    if (WiFi.isConnected())
+    {
         init_ntp();
+        ntp_setup = true;
+        delay(5000);
     }
 }
 
 void loop()
 {
-    if (!is_setup())
-    {
-        serial_setup();
-    }
-
-    if (!is_wifi_setup())
-    {
-        serial_wifi_setup();
-    }
-
     if (is_wifi_setup())
     {
-        wifi_connect();
+        if (!WiFi.isConnected())
+        {
+            wifi_connect();
+        }
+
+        if (!ntp_setup && WiFi.isConnected())
+        {
+            init_ntp();
+            delay(5000);
+        }
     }
 
-    if(WiFi.isConnected()) {
-        init_ntp();
-    }
+    time(&last_measurement);
+    bool day_update = read_sensor_values();
 
-    read_sensor_values();
-
-    print_data_to_serial();
+    print_data_to_serial(!day_update);
 
     // TODO: broadcast data to UDP
 
     // TODO: webserver
 
-    // TODO: handle with timer interrupt for more precise distances
-    delay(SLEEP_TIME);
+    time_t now;
+    do
+    {
+        time(&now);
+        delay(200);
+    } while (now < (last_measurement + SLEEP_TIME));
 }
 
-void read_sensor_values(void)
+bool read_sensor_values(void)
 {
     float humidity = dht.readHumidity();
     float temperature = dht.readTemperature();
-    add_values(humidity, temperature);
+    return add_values(humidity, temperature);
 }
